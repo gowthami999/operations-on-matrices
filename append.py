f2b6bd18536bc16bf73251e5c1bd78e1c29c13c1
gowthami999 <58940446+gowthami999@users.noreply.github.com>
2019-12-19 19:02:40 +0530

    Add files via upload

---
+++ append.py
@@ -0,0 +1,8 @@
+import numpy as np
+x=np.array(input("enter the list values"))
+y=np.array(input("enter the another list values which has to append"))
+z=np.append(x,[y])
+print('the given values are:',x)
+print('the new are are which has to append:',y)
+print('after appending the new values are:',z)
+
